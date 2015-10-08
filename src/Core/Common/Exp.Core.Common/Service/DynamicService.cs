7791735bd7f5f12df9bb98fe5515855c832fff46
pedrofernandesfilho <pedro.fernandesfilho@gmail.com>
2015-10-08 00:43:32 -0300

    Rename Test to Dynamic

---
+++ src/Core/Common/Exp.Core.Common/Service/DynamicService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Exp.Core.Common.Service
+{
+    public class DynamicService
+    {
+        public IEnumerable<dynamic> Get()
+        {
+            List<dynamic> r = new List<dynamic>();
+            dynamic item;
+            
+            for (int i = 10; i < 17; i++)
+            {
+                item = new ExpandoObject();
+
+                item.Id = i;
+                item.PropA = "prop a";
+                item.PropB = "prop b";
+                item.PropC = 456;
+
+                r.Add(item);
+            }
+
+            return r;
+        }
+
+        public dynamic Get(int id)
+        {
+            dynamic r = new ExpandoObject();
+
+            r.Id = id;
+            r.PropA = "prop a";
+            r.PropB = "prop b";
+            r.PropC = 456;
+
+            return r;
+        }
+    }
+}
--- src/UI/Exp.UI.WebAPI/Controllers/DynamicController.cs
+++ src/UI/Exp.UI.WebAPI/Controllers/DynamicController.cs
@@ -4,28 +4,28 @@ using System.Web.Http;
 
 namespace Exp.UI.WebAPI.Controllers
 {
-    [RoutePrefix("api/Test")]
-    public class TestController : ApiController
+    [RoutePrefix("api/Dynamic")]
+    public class DynamicController : ApiController
     {
-        private readonly TestService testService = new TestService();
+        private readonly DynamicService testService = new DynamicService();
         
         // GET: api/Test
         public IEnumerable<dynamic> Get()
         {
             return testService.Get();
         }
-        
-        [Route("Named")]
-        public object GetNamed()
-        {
-            object r = new { Items = testService.Get() };
-            return r;
-        }
 
         // GET: api/Test/5
         public dynamic Get(int id)
         {
             return testService.Get(id);
+        }
+
+        [Route("Named")]
+        public object GetNamed()
+        {
+            object r = new { Items = testService.Get() };
+            return r;
         }
     }
 }
