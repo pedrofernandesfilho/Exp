c333690f9951455fad703713c4ca75d3f5b23dc3
pedrofernandesfilho <pedro.fernandesfilho@gmail.com>
2015-10-08 00:12:14 -0300

    Conn

---
+++ src/Infra/Data/Exp.Infra.Data.ADO/Conn.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data.Common;
+
+namespace Exp.Infra.Data.ADO
+{
+    public class Conn
+    {
+        private readonly DbConnection _conn;
+
+        public Conn(string connectionString)
+        {
+            ConnectionStringSettings css = ConfigurationManager.ConnectionStrings[connectionString];
+
+            DbProviderFactory dbProviderFactory = DbProviderFactories.GetFactory(css.ProviderName);
+
+            _conn = dbProviderFactory.CreateConnection();
+        }
+    }
+}
